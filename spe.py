a0d15cbe6eb863de089f5873ab534649db7badda
dlsnoopy95052 <32687526+dlsnoopy95052@users.noreply.github.com>
2018-02-10 19:34:51 -0800

    clean up redudant code

--- spe.py
+++ spe.py
@@ -17,38 +17,24 @@ def main():
 	if (sys.argv[3]=="-en"):
 		print("gonna do encryption...")
 		clearTextPassword=sys.argv[4]
-		print("encrypted to ",encry(key,clearTextPassword))
+		print("encrypted to ",encry_decry(key,clearTextPassword,True))
 	elif (sys.argv[3]=="-de"):
 		print("gonna do decryption...")
 		encryptedPassword=sys.argv[4]
-		print("decrypt to ",decry(key,encryptedPassword))
+		print("decrypt to ",encry_decry(key,encryptedPassword,False))
 	else:
 		print("wrong syntax. you have to use '-en' or '-de'")
 		sys.exit()
 	
 
-	#print("key is {}".format(key))
-
-def encry(key,ctpwd):
-	if(len(key)!=len(ctpwd)):
-		adjustedkey=adjustkey(key,len(ctpwd))
+def encry_decry(key,pwd,en):
+	if(len(key)!=len(pwd)):
+		adjustedkey=adjustkey(key,len(pwd))
 	else:
 		adjustedkey=key
-	#print("adjusted key is",adjustedkey)
-	#encrykey = bin(ctpwd) ^ bin(adjustedkey)
-	encrypwd=dothejob(adjustedkey,ctpwd,True)
-	return(encrypwd)
+	newpwd=dothejob(adjustedkey,pwd,en)
+	return newpwd
 
-def decry(key,enpwd):
-	if(len(key)!=len(enpwd)):
-		adjustedkey=adjustkey(key,len(enpwd))
-	else:
-		adjustedkey=key
-	#print("adjusted key is",adjustedkey)
-	#encrykey = bin(ctpwd) ^ bin(adjustedkey)
-	decrypwd=dothejob(adjustedkey,enpwd,False)
-	return(decrypwd)
-	
 
 def adjustkey(key,leng):
 	if (len(key)>leng):
@@ -57,7 +43,6 @@ def adjustkey(key,leng):
 	else:
 		#increase key size
 		factor=int(leng/len(key))
-		#print("factor is",factor)
 		tempnewkey=key*(factor+1)
 		return tempnewkey[:leng]
 
@@ -65,15 +50,14 @@ def dothejob(key,pwd,en):
 	keylist=[]
 	pwdlist=[]
 	resultlist=[]
-	#print("gonna do the job")
+
 	for i in range(0,len(key)):
 		if (en==True):
 			result=(ord(key[i])^ord(pwd[i]))+33
 		else:
 			result=(ord(key[i])^(ord(pwd[i])-33))
 		resultlist.append(chr(result))
-		#print(result)
-	#print(resultlist)
+
 	password=""
 	for j in resultlist:
 		password += j
