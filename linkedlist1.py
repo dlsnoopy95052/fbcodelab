6d2b86b2a940976b3c7855c7d8ec52f8af06503b
dlsnoopy95052 <32687526+dlsnoopy95052@users.noreply.github.com>
2018-02-12 00:23:10 -0800

    add more

--- linkedlist1.py
+++ linkedlist1.py
@@ -18,7 +18,58 @@ class LinkedList(object):
 			newNode.nextNode = self.head
 			self.head = newNode
 
+	def remove(self,data):
+		if self.head is None:
+			return
+		self.size = self.size -1
+		currentNode = self.head
+		previousNode = None
+		print("to remove {}".format(data))
+		while currentNode.data != data:
+			previousNode = currentNode
+			currentNode = currentNode.nextNode
 
-	def size(self):
+		if previousNode is None:
+			self.head = currentNode.nextNode
+		else:
+			#print("***",currentNode.data, previousNode.data)
+			previousNode.next = currentNode.nextNode
+
+	def sizeoflist(self):
 		return self.size
 
+	def insertEnd(self,data):
+		newNode = Node(data)
+		self.size += 1
+		actualNode = self.head
+		if self.head is None:
+			self.head = newNode
+			return
+		while actualNode.nextNode is not None:
+			actualNode = actualNode.nextNode
+
+		actualNode.nextNode = newNode
+		#newNode.nextNode = None
+
+	def traverseList(self):
+		actualNode = self.head
+		while actualNode is not None:
+			print("%d " %actualNode.data)
+			actualNode = actualNode.nextNode
+
+
+
+linkedlist = LinkedList()
+linkedlist.insertEnd(12)		
+linkedlist.insertEnd(122)
+linkedlist.insertStart(3)
+linkedlist.insertEnd(31)
+linkedlist.traverseList()
+print(linkedlist.sizeoflist())
+
+linkedlist.remove(12)
+linkedlist.remove(12)
+linkedlist.remove(1)
+linkedlist.remove(31)
+linkedlist.traverseList()
+print(linkedlist.sizeoflist())
