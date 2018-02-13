910f11a367ca15d9e571bdf9b76d1a7d1674467f
dlsnoopy95052 <32687526+dlsnoopy95052@users.noreply.github.com>
2018-02-13 09:57:59 -0800

    fixed a bug in remove function

--- linkedlist1.py
+++ linkedlist1.py
@@ -12,7 +12,7 @@ class LinkedList(object):
 		self.size = self.size + 1
 		newNode = Node(data)
 
-		if not self.head:
+		if self.head is None:
 			self.head = newNode
 		else:
 			newNode.nextNode = self.head
@@ -21,22 +21,35 @@ class LinkedList(object):
 	def remove(self,data):
 		if self.head is None:
 			return
-		self.size = self.size -1
+		#self.size = self.size -1
 		currentNode = self.head
 		previousNode = None
-		print("to remove {}".format(data))
-		while (currentNode is not None) and (currentNode.data != data):
+		#print("to remove {}".format(data))
+		while (currentNode.data != data):
 			previousNode = currentNode
 			currentNode = currentNode.nextNode
-
+			if (currentNode is None):
+				print("cannot find {} in the list".format(data))
+				return
+		#print("currentNode data is ", currentNode.data)
 		if previousNode is None:
+			#print("case 1")
 			self.head = currentNode.nextNode
+			self.size -= 1
 		else:
 			#print("***",currentNode.data, previousNode.data)
 			if currentNode is None:
+				#print("case 2")
 				print("cannot delete {}".format(data))
 			else:
-				previousNode.next = currentNode.nextNode
+				#print("case 3")
+				#print("***",previousNode.data,currentNode.data)
+				previousNode.nextNode = currentNode.nextNode
+				#if (previousNode.next is None):
+					#print("previous's next is none??")
+				#print("currentnode data is ", previousNode.nextNode)
+				#print("&&&",(previousNode.next).data)
+				self.size -= 1
 
 	def sizeoflist(self):
 		return self.size
@@ -56,6 +69,9 @@ class LinkedList(object):
 
 	def traverseList(self):
 		actualNode = self.head
+		if (actualNode is None):
+			print("empty list")
+			return
 		while actualNode is not None:
 			print("%d " %actualNode.data)
 			actualNode = actualNode.nextNode
@@ -63,16 +79,21 @@ class LinkedList(object):
 
 
 linkedlist = LinkedList()
-linkedlist.insertEnd(12)		
-linkedlist.insertEnd(122)
+linkedlist.insertStart(12)	
+linkedlist.insertStart(122)
+linkedlist.insertStart(12)
 linkedlist.insertStart(3)
 linkedlist.insertEnd(31)
+print("traversing starts")
 linkedlist.traverseList()
-print(linkedlist.sizeoflist())
+print("traversing is done")
+print("total size is {}".format(linkedlist.sizeoflist()))
 
 linkedlist.remove(12)
 linkedlist.remove(12)
-linkedlist.remove(1)
-linkedlist.remove(31)
+linkedlist.remove(122)
+#linkedlist.remove(31)
+print("traversing starts")
 linkedlist.traverseList()
-print(linkedlist.sizeoflist())
+print("traversing is done")
+print("total size is {}".format(linkedlist.sizeoflist()))
